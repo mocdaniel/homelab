bc3902aa8702b3388c89bbaffa011e937901d4ca
Daniel Bodky <dbodky@gmail.com>
2024-01-13 20:21:04 +0100

    Adds domain for Dashy

--- opentofu/domains.tf
+++ opentofu/domains.tf
@@ -25,4 +25,10 @@ resource "namecheap_domain_records" "bodkys-house" {
     type = "A"
     address = "185.11.255.65"
   }
+
+  record {
+    hostname = "dashy"
+    type = "A"
+    address = "185.11.255.65"
+  }
 }
